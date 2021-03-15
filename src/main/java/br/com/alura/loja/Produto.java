76af039e6778b1448475945c1fd0e4ff1329f184
brunamello <brunacrespomello@gmail.com>
2021-03-15 12:06:15 +0000

    05. Outros Recursos ( plugins, modulos, proxy)

--- src/main/java/br/com/alura/loja/Produto.java
+++ src/main/java/br/com/alura/loja/Produto.java
@@ -1,5 +1,29 @@
 package br.com.alura.loja;
 
+import java.math.BigDecimal;
+
 public class Produto {
+
+    String nome = "Bruna";
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    double preco = 10.00;
+
+
 
 }
--- src/test/java/br/com/alura/loja/ProdutoTest.java
+++ src/test/java/br/com/alura/loja/ProdutoTest.java
@@ -2,13 +2,20 @@ package br.com.alura.loja;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.math.BigDecimal;
 
 public class ProdutoTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+
+		Produto p = new Produto("teste", BigDecimal.TEN);
+		Assert.assertArrayEquals("teste", p.getNome());
+		Assert.assertArrayEquals(BigDecimal.TEN, p.getPreco());
+
 	}
 
 }
